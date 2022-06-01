7247e4be0c78301ab487101977df7c57d1fa0097
Katherine_20 <mob1911494@gmail.com>
2022-06-01 18:10:33 +0300

    фиксация: "master"

---
+++ рекурсия пример/Program.cs
@@ -0,0 +1,14 @@
+﻿// 5! = 5*4*3*2*1
+//    = 5*4!
+//        4*3!
+//          3*2!
+double Factorial (int n)
+{
+    if(n == 1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+ 
